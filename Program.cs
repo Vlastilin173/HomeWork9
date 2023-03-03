0e764369bd8c64dabaaeefb3f9fde9f09bf8bd15
Vladimir <vova_spirid@mail.ru>
2023-03-04 01:19:07 +0400

    pull request

--- Program.cs
+++ Program.cs
@@ -19,3 +19,16 @@ Console.Write($"M = {M}; N = {N} -> {SummaNaturalotMdoN(M, N)}");
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+int Function(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return Function(m - 1, 1);
+    else return (Function(m - 1, Function(m, n - 1)));
+}
+
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write($"m = {m}, n = {n} -> A(m,n) = {Function(m, n)}");
